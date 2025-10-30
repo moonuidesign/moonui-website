58b0fed7d54b46787a9edc5f683cfad88d93d7c2
FajarFE <fajarfernandi123123@gmail.com>
2025-10-30 19:10:46 +0700

    fixed

--- ssr/drizzle.config.ts
+++ ssr/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './db/output',
-  schema: './drizzle/schema.ts',
+  schema: './db/migration/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
